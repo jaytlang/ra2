07e5070876f719a523f60997cc47ec2f95f2355b
Jay Lang <jaytlang@mit.edu>
2022-02-02 18:06:21 -0500

    Calculate team stats

--- stats.go
+++ stats.go
@@ -123,6 +123,53 @@ func (t *stats) rptSPerT() {
 	fmt.Printf("Students per tutorial: %d\n", len(t.l)/len(tuts)+allowedTutOvf)
 }
 
+func (t *stats) rptTeamSatisfaction() {
+	pt := 0
+
+	perfs := 0
+	pairs := 0
+	singles := 0
+
+	for _, fn := range t.l {
+		favs := fn.st.dpfavs
+		team := fn.team
+
+		wanted := len(fn.st.dpfavs)
+		if wanted == 0 {
+			continue
+		}
+
+		pt++
+		got := 0
+
+		for _, fav := range favs {
+			for _, tm := range team {
+				if fav == tm {
+					got++
+				}
+			}
+		}
+
+		if got == 2 {
+			perfs++
+		} else if got == 1 {
+			if wanted == 2 {
+				pairs++
+			} else {
+				perfs++
+			}
+		} else {
+			singles++
+		}
+	}
+
+	fmt.Printf("Team preferences (percentages calced only with students who wanted teams):\n")
+	fmt.Printf("\tStudents getting all of their team preferences: %s (%d)\n", percent{perfs, pt}, perfs)
+	fmt.Printf("\tStudents getting one of their two team preferences: %s (%d)\n", percent{pairs, pt}, pairs)
+	fmt.Printf("\tStudents getting none of their team preferences: %s (%d)\n", percent{singles, pt}, singles)
+	fmt.Printf("\t[Students with no team preferences: %d]\n", len(t.l)-pt)
+}
+
 func (t *stats) prepare(l []*fn) error {
 	t.l = l
 	return nil
@@ -137,6 +184,8 @@ func (t *stats) execute() error {
 	t.rptSPerT()
 	t.rptTutEnrollment()
 	t.rptRecEnrollment()
+
+	t.rptTeamSatisfaction()
 
 	return nil
 }
